28470c5231051c0b563ac96cd34e0e6a7ec04467
marianrr <marianseether@gmail.com>
2024-08-12 22:02:09 +0300

    adaugare tabel

--- app/crudmysql/page.tsx
+++ app/crudmysql/page.tsx
@@ -4,11 +4,24 @@ import executeQuery from "./mysqldb"
  const Page = async () =>  {
 
 const result = await executeQuery("select * from student", [])
-
+console.log("Rezultatul este:", result)
     return (
     
     <div>
     {JSON.stringify(result)}
+    <table>
+       <tbody>
+        <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Location</th>
+            <th>Id</th>
+        </tr>
+        {result.map(h => (<tr key={h.id}><td>{h.name}</td><td>{h.age}</td><td>{h.location}</td><td>{h.id}</td></tr>))}
+
+
+        </tbody>
+    </table>
     
     </div>
     
